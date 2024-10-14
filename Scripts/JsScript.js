6627144f25469625fc957746279e9749dfd7fdb1
AmarjitSinghCodeHub <amarjitsinghgigs@gmail.com>
2024-10-14 15:12:15 +0530

    First Commit

---
+++ Scripts/JsScript.js
@@ -0,0 +1,6 @@
+import {abc, msg,ShowMsg} from './Parent.js';
+console.log(msg);
+ShowMsg();
+console.log(abc);
+
+
---
+++ Scripts/Parent.js
@@ -0,0 +1,6 @@
+let msg="this is message";
+const abc="This is abc const variable";
+function ShowMsg(){
+    console.log("This message is from function");
+}
+export {msg,ShowMsg,abc};
